6ffa656e4fd56a5c79fdfa56adcf8e496c0942ec
Jessid <jvelez@sadako.es>
2020-11-23 20:08:27 +0100

    Deleted lots and lots of files created in the cmake and make processes

--- PatternsInCpp/OpenClosed/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jessid/VSCodeProjects/PatternsInCpp/OpenClosed
-# Build directory: /home/jessid/VSCodeProjects/PatternsInCpp/OpenClosed
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- PatternsInCpp/OpenClosed/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jessid/VSCodeProjects/PatternsInCpp/OpenClosed
-# Build directory: /home/jessid/VSCodeProjects/PatternsInCpp/OpenClosed/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
